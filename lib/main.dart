215111f08a13b17a6290a4f3f35f339efcfc4fc9
datsabahandude <mohd.noor.amlee@gmail.com>
2022-12-25 11:09:21 +0800

    change theme and set bg image #1

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Kambing Pak Dola'),
     );
@@ -32,15 +32,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -60,17 +51,11 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Container(
       padding: EdgeInsets.all(20.0),
         decoration: BoxDecoration(
-        gradient: LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [
-        Color(0xffe3f2e1),
-        Color(0xcce3f2e1),
-        Color(0x99e3f2e1),
-        Color(0x66e3f2e1),
-        ]
-        )
-        ),// This trailing comma makes auto-formatting nicer for build methods.
+          image: DecorationImage(
+            image: AssetImage("images/goated.jpg"),
+            fit: BoxFit.cover
+          )
+        ),
       ),
     );
   }
