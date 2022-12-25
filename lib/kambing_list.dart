3bbdd7a68795ce1c95f031dd24a09735bdfd2410
datsabahandude <mohd.noor.amlee@gmail.com>
2022-12-25 21:17:48 +0800

    [in-progress] #3

---
+++ lib/kambing_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class kambing_list extends StatefulWidget {
+  const kambing_list({Key? key}) : super(key: key);
+
+  @override
+  State<kambing_list> createState() => _kambing_listState();
+}
+
+class _kambing_listState extends State<kambing_list> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/penjantan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class penjantan extends StatefulWidget {
+  const penjantan({Key? key}) : super(key: key);
+
+  @override
+  State<penjantan> createState() => _penjantanState();
+}
+
+class _penjantanState extends State<penjantan> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
