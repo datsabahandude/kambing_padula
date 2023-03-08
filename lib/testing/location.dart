f109e53d08002b7aa4aca995ade04dafc1c9856a
datsabahandude <mohd.noor.amlee@gmail.com>
2023-03-08 21:35:08 +0800

    fix launchUrl

--- lib/testing/location.dart
+++ lib/testing/location.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class LocationPage extends StatefulWidget {
   const LocationPage({super.key});
@@ -9,8 +10,9 @@ class LocationPage extends StatefulWidget {
 }
 
 class _LocationPageState extends State<LocationPage> {
-  late String lat;
-  late String long;
+  final Uri _url = Uri.parse('https://github.com/datsabahandude');
+  String lat = '';
+  String long = '';
   String locationMessage = '';
   bool isLoading = false;
   @override
@@ -46,15 +48,27 @@ class _LocationPageState extends State<LocationPage> {
                                 'Latitude: $lat\nLongitude: $long';
                             isLoading = false;
                           });
+                          // _liveLocation();
                         });
                       },
                       child: const Text('Get Current Location'),
-                    )
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                        onPressed: lat.isEmpty
+                            ? () {}
+                            : () {
+                                _openMap(lat, long);
+                              },
+                        child: const Text('Open Map'))
                   ]),
             ),
     );
   }
 
+  /// Permission to use GPS
   Future<Position> _getCurrentLocation() async {
     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
@@ -71,6 +85,29 @@ class _LocationPageState extends State<LocationPage> {
       return Future.error(
           'Location Permission Permanently Disabled, Please Enable');
     }
-    return await Geolocator.getCurrentPosition();
+    return await Geolocator.getCurrentPosition(); // Fetch Current Position
+  }
+
+  void _liveLocation() {
+    LocationSettings locationSettings = const LocationSettings(
+        accuracy: LocationAccuracy.high, distanceFilter: 100);
+    Geolocator.getPositionStream(locationSettings: locationSettings)
+        .listen((Position position) {
+      lat = position.latitude.toString();
+      long = position.longitude.toString();
+      setState(() {
+        locationMessage = 'Latitude: $lat\nLongitude: $long';
+      });
+    });
+  }
+
+  /// Open current location in Google Map
+  Future<void> _openMap(String lat, String long) async {
+    // refer: https://developers.google.com/maps/documentation/urls/get-started
+    String googleUrl =
+        'https://www.google.com/maps/search/?api=1&query=$lat,$long';
+    await canLaunchUrlString(googleUrl)
+        ? await launchUrlString(googleUrl)
+        : throw 'Could Not Launch $googleUrl';
   }
 }
