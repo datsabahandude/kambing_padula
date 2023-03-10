1bc385f7ba78be920eb5b2fb7a4ca3b7c780b911
datsabahandude <mohd.noor.amlee@gmail.com>
2023-03-10 11:12:01 +0800

    added qr generator and scanner [virus warning] #25

---
+++ lib/testing/qr_scan.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class QRScanPage extends StatefulWidget {
+  const QRScanPage({super.key});
+
+  @override
+  State<QRScanPage> createState() => _QRScanPageState();
+}
+
+class _QRScanPageState extends State<QRScanPage> {
+  String qrCode = 'Unknown';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('QR Scanner'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Text('Scan Result'),
+            const SizedBox(height: 8),
+            Text(qrCode),
+            ElevatedButton(
+              onPressed: () => scanQRCode(),
+              child: const Text('Scan'),
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+
+  Future<void> scanQRCode() async {
+    try {
+      final qrCode = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      if (!mounted) return;
+      setState(() {
+        this.qrCode = qrCode;
+      });
+    } on PlatformException {
+      qrCode = 'Failed';
+      Fluttertoast.showToast(
+          msg: qrCode,
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
+  }
+}
